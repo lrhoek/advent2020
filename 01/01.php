3bf2ebad6fe5543e2866e0a436fb89e815425c13
Laurens Hoek <lrhoek@gmail.com>
2022-01-20 19:39:04 +0100

    day 1 cleanup

--- 01/01.php
+++ 01/01.php
@@ -1,19 +1,18 @@
 <?php
 
-function findSum($numbers, $sum) {
-    $check = array_pop($numbers);
-    $found = in_array($sum - $check, $numbers);
-    return ($found ? [(int) $check, $sum - $check] : (empty($numbers) ? [] : findSum($numbers, $sum)));
+function findSum(array $numbers, int $sum) : array {
+    $current = array_pop($numbers);
+    $check = $sum - $current;
+    return in_array($check, $numbers) ? [(int) $current, $check] : (empty($numbers) ? [] : findSum($numbers, $sum));
 }
 
-function findSumOfThree($numbers, $sum) {
+function findSumOfThree(array $numbers, int $sum) : array {
     $check = array_pop($numbers);
     $found = findSum($numbers, $sum - $check);
-    return (!empty($found) ? array_merge([(int) $check], $found) : findSumOfThree($numbers, $sum));
+    return !empty($found) ? [(int) $check, ...$found] : findSumOfThree($numbers, $sum);
 }
 
 $numbers = explode(PHP_EOL, file_get_contents('input'));
 
 echo array_product(findSum($numbers, 2020)).PHP_EOL;
-
 echo array_product(findSumOfThree($numbers, 2020)).PHP_EOL;
